8f51f358e34de54f22314310314d092e0abe5cad
isa <isaqurbanov112@gmail.com>
2026-03-01 11:49:19 +0400

    first commit

---
+++ src/main/java/com/bmu1093a/quill/QuillApplication.java
@@ -0,0 +1,13 @@
+package com.bmu1093a.quill;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuillApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuillApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bmu1093a/quill/QuillApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bmu1093a.quill;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuillApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
