84644c874c030434aaeead6c6a4844f723e8912d
isa <isaqurbanov112@gmail.com>
2026-03-10 16:14:53 +0400

    switched to modular architecture

---
+++ src/main/java/com/bmu1093a/quill/authentication/controller/AuthenticationController.java
@@ -0,0 +1,7 @@
+package com.bmu1093a.quill.authentication.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class AuthenticationController {
+}
